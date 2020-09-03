44e4e12a0a460e883927a99d5a54011d70682bf1
doktorfish0078 <68699739+doktorfish0078@users.noreply.github.com>
2020-09-03 14:51:11 +0400

    asd

--- Include/Commands/schedule_bus.py
+++ Include/Commands/schedule_bus.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from PIL import Image
+from io import BytesIO
 
 import datetime
 import re
-import os
+
 
 
 def get_path_schedule_bus(text_msg):
@@ -11,33 +12,31 @@ def get_path_schedule_bus(text_msg):
     :param text_msg:
     :return: Возвращает путь до сделанного скриншота
     """
-    path_to_chromdriver = ""
+
     now = datetime.datetime.now()  # текущая дата и время
     URL = "https://igis.ru/gortrans/bus/izh/"  # izh/номер автобуса
     numb_bus = get_numb_bus(text_msg)
     if numb_bus is None and 2 <= numb_bus <= 400:
         return None
     URL += numb_bus
-    print(URL)
-
-
-    #chrome_options = Options()
-    #chrome_options.add_argument("--headless")
-    #driver = webdriver.Chrome(options=chrome_options)
-    #firefox_binary = os.environ.get('FIREFOX_BIN', None)
-    #opt = webdriver.FirefoxOptions()
-    #opt.add_argument("--headless")
-    #driver = webdriver.Firefox(options=opt)
-    #driver.set_window_size(1600, 2070)
-    #driver.get(URL)
-    #elem = driver.find_element_by_class_name("table-st1")
-    #elem.screenshot(
-    #    "screenshots\screen_" + now.strftime("%d-%m-%Y %H-%M-%S") + ".png")
-    #driver.quit()
-    #return "screenshots\screen_{0}.png".format(
-       # now.strftime("%d-%m-%Y %H-%M-%S"))
-        #print("Ошибка получения скриншота с адреса " + URL)
-        #return "None"
+    try:
+        driver = webdriver.PhantomJS()
+        driver.set_window_size(1600, 2070)
+        driver.get(URL)
+        elem = driver.find_element_by_class_name("table-st1")
+        screen = driver.get_screenshot_as_png()
+        rect = elem.rect  # {'height': 1041, 'width': 722, 'x': 562, 'y': 689}
+        box = (rect['x'], rect['y'], rect['x'] + rect['width'], rect['y'] + rect['height'])
+        im = Image.open(BytesIO(screen))
+        region = im.crop(box)  # вырезаем
+        region.save("screenshots\screen_" + now.strftime("%d-%m-%Y %H-%M-%S") + ".png", 'PNG')
+        driver.quit()
+        print("Удачная загрузка скриншота")
+        return "screenshots\screen_{0}.png".format(
+            now.strftime("%d-%m-%Y %H-%M-%S"))
+    except BaseException:
+        print("Ошибка получения скриншота с адреса " + URL)
+        return "None"
 
 
 def get_numb_bus(text_msg):
--- Include/Commands/testTrasd.py
+++ Include/Commands/testTrasd.py
@@ -2,29 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import datetime
 from PIL import Image
-from io import StringIO
+from io import BytesIO
 
 def get_schedule_bus():
     now = datetime.datetime.now()
-    URL = "https://igis.ru/gortrans/bus/izh/29"
+    URL = "https://igis.ru/gortrans/bus/izh/19"
     driver = webdriver.PhantomJS()
-    #driver.set_window_size(1600, 2070)
+    driver.set_window_size(1600, 2070)
     driver.get(URL)
     elem = driver.find_element_by_class_name("table-st1")
-    driver.save_screenshot("screenshots\screen_" + now.strftime("%d-%m-%Y %H-%M-%S") + ".png")
-
     screen = driver.get_screenshot_as_png()
-
-    print(elem.location)
-    print(elem.size)
-    box = (elem.location['x'], elem.location['y'], elem.size['height'], elem.size['width'])
-    im = Image.open(StringIO(screen))
-    region = im.crop(box)
-    region.save("screenshots\screen_" + now.strftime("%d-%m-%Y %H-%M-%S") + ".png", 'JPEG', optimize=True, quality=95)
-    #elem.screenshot("screenshots\screen_" + now.strftime("%d-%m-%Y %H-%M-%S") + ".png")
-    #elem.screenshot(
-        #"screenshots\screen_" + now.strftime("%d-%m-%Y %H-%M-%S") + ".png")
-    #driver.save_screenshot("screenshots\screen_" + now.strftime("%d-%m-%Y %H-%M-%S") + ".png")
+    rect = elem.rect  # {'height': 1041, 'width': 722, 'x': 562, 'y': 689}
+    box = (rect['x'], rect['y'], rect['x'] + rect['width'], rect['y'] + rect['height'])
+    im = Image.open(BytesIO(screen))
+    region = im.crop(box) # вырезаем
+    region.save("screenshots\screen_" + now.strftime("%d-%m-%Y %H-%M-%S") + ".png", 'PNG')
     driver.quit()
     print("Удачно")
 get_schedule_bus()
