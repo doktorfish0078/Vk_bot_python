ab9f667f70ba815795666f48c365d93a5d889c08
liqloi <34963507+liqloi@users.noreply.github.com>
2020-09-04 14:47:45 +0400

    Говна поешь

--- Include/Commands/schedule.py
+++ Include/Commands/schedule.py
@@ -1,3 +1,3 @@
+from random import randint
 def schedule():
-    return "photo-186084635_457239280"
-
+    return "photo-186084635_457239280" if randint(1, 100) > 10 else "photo-186084635_457239147"
