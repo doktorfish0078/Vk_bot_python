f70c5f4d5171ee724aef54fa5b3b50f21451758a
liqloi <34963507+liqloi@users.noreply.github.com>
2020-09-08 14:26:00 +0400

    а

--- Include/VkBotServer.py
+++ Include/VkBotServer.py
@@ -114,14 +114,16 @@ def parse_msg(event):
     elif 'спасибо' in msg_text:
         send_msg_tochat(chat_id, 'Если чем-то помог, то пожалуйста:3' if random.randint(1, 5) > 1 else 'Иди нахуй')
     elif ('привет' in msg_text or 'здравствуй' in msg_text) and ('сладкий' in msg_text or 'бот' in msg_text) and \
-                    ((greeted[event.message['from_id']] - time()) > 6000 if event.message['from_id'] in greeted.keys() else True):
+                    ((greeted[event.message['from_id']] - time()) > 600 if event.message['from_id'] in greeted.keys() else True):
         greeted[event.message['from_id']] = time()
         if event.message['from_id'] == ortom_id:
             hello = greet.ortom_hello()
         else:
             hello = greet.hello()
-        send_msg_tochat(chat_id, hello.format(
-            vk_session.method('users.get', {'user_ids': event.message['from_id']})[0]['first_name']))
+        send_msg_tochat(chat_id,
+                        hello.format(
+                            vk_session.method('users.get', {'user_ids': event.message['from_id']})[0]['first_name'])
+                        )
     elif 'аниме' in msg_text:
         send_msg_tochat(chat_id, myanimelist.get_top())
     elif 'секс' in msg_text:
