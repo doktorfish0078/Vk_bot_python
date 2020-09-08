b1f23c7fd67e6aa8bf50180cfcab27c01a2618f2
liqloi <34963507+liqloi@users.noreply.github.com>
2020-09-08 14:14:20 +0400

    Update greet.py

--- Include/Commands/greet.py
+++ Include/Commands/greet.py
@@ -1,12 +1,14 @@
 from random import randint
 
+smiles = [':>', 'O-o', '☾☽∪☾☽', '❪❫❣❪❫', ':-)', '⊙⊔⊙', '⊝o⊝', '∘∨∘']
+
 def ortom_hello():
     ortom_hello_str = ['Скучал по твоей попе, {0}', 'Люблю твои хоум видео, {0}', 'Пасаси, {0}',
                        'Привет твоей маме, {0}', 'Попу надо мыть, {0}']
     return ortom_hello_str[randint(0, len(ortom_hello_str) - 1)]
 
 def hello():
-    hello_str = ['Приветик, {0}', 'Здравствуй, О {0}', 'Снова ты, {0}?',
-                 'Уже вернулись, {0}?', 'Хо-хо, а я тебя не ждал, {0}'
-                 '{0}, какая встреча!']
-    return hello_str[randint(0, len(hello_str) - 1)]
+    hello_str = ['Приветик, {0} ', 'Здравствуй, О {0} ', 'Снова ты, {0}? ',
+                 'Уже вернулись, {0}? ', 'Хо-хо, а я тебя не ждал, {0} '
+                 '{0}, какая встреча! ']
+    return hello_str[randint(0, len(hello_str))] + smiles[randint(0, len(hello_str))]
