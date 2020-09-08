81ceea15eac92ca708ae14bdb84201a48f358477
doktorfish0078 <68699739+doktorfish0078@users.noreply.github.com>
2020-09-08 19:20:12 +0400

    asd

--- Include/Commands/weather.py
+++ Include/Commands/weather.py
@@ -2,89 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 
 
-parts = ['morning','day','evening','night']
-url = 'https://api.weather.yandex.ru/v2/forecast?'
-api_key_yandex = 'c585325e-939a-4e62-bfb1-af081d21f2ea'
-lat=56.86186
-lon=53.23243
-headers = {
-    'X-Yandex-API-Key': 'c585325e-939a-4e62-bfb1-af081d21f2ea'
-}
-params = {
-    'lat': 'lat',
-    'lon': 'lon'
-}
-
-
-def weather_td():
+def weather(tomorrow = False):
     timeday = ['Утром ', 'Днём ', 'Вечером ', 'Ночью ']
-
+    numb_card_for_parsing = 0
+    day = 'сегодня'
+    if tomorrow:
+        numb_card_for_parsing = 2
+        day = 'завтра'
     try:
-        out_weather = 'Погода на сегодня:\n'
         html_text = requests.get('https://yandex.ru/pogoda/izhevsk/details?via=ms').text
         soup = BeautifulSoup(html_text, features="html.parser")
         weather = soup.find_all('div', {'class', 'card'})
-        temp = weather[0].find_all('span', {'class', 'temp__value'})
+        weather_condition = weather[numb_card_for_parsing].find_all('td', {'class',
+                                                       'weather-table__body-cell weather-table__body-cell_type_condition'})
+        out_weather = ['Погода на {}:\n'.format(day), weather_condition[2].text]
+        temp = weather[numb_card_for_parsing].find_all('span', {'class', 'temp__value'})
         for day in range(len(timeday)):
-            for i in range(day * 3,(day + 1) * 3):
+            timeday[day] += '{} '.format(weather_condition[day].text)
+            for i in range(day * 3, (day + 1) * 3):
                 if day * 3 + 1 == i:
                     timeday[day] += '...'
                 elif day * 3 + 2 == i:
                     timeday[day] += ' ощущается как '
                 timeday[day] += temp[i].text + 'ºC'
-            out_weather += timeday[day] + '\n'
+            out_weather[0] += timeday[day] + '\n'
         return out_weather
     except BaseException:
         return 'pezda pogode'
 
-
-def weather_tm():
-    timeday = ['Утром ', 'Днём ', 'Вечером ', 'Ночью ']
-
-    try:
-        out_weather = 'Погода на завтра:\n'
-        html_text = requests.get('https://yandex.ru/pogoda/izhevsk/details?via=ms').text
-        soup = BeautifulSoup(html_text, features="html.parser")
-        weather = soup.find_all('div', {'class', 'card'})
-        temp = weather[2].find_all('span', {'class', 'temp__value'})
-        for day in range(len(timeday)):
-            for i in range(day * 3,(day + 1) * 3):
-                if day * 3 + 1 == i:
-                    timeday[day] += '...'
-                elif day * 3 + 2 == i:
-                    timeday[day] += ' ощущается как '
-                timeday[day] += temp[i].text + 'ºC'
-            out_weather += timeday[day] + '\n'
-        return out_weather
-    except BaseException:
-        return 'pezda pogode'
-
-
-def weater_api_yandx_today():
-    out_weather = 'Погода на сегодня:\n'
-    try:
-        html_text = requests.get(url,params=params,headers = headers).json()
-        for part in parts:
-            temp_min = html_text['forecasts'][0]['parts'][part]['temp_min']
-            temp_max = html_text['forecasts'][0]['parts'][part]['temp_max']
-            feels_like = html_text['forecasts'][0]['parts'][part]['feels_like']
-            f = '{0} {1}ºC...{2}ºC feels like {3}ºC\n'.format(part, temp_min, temp_max, feels_like)
-            out_weather += f
-        return out_weather
-    except BaseException:
-        return 'Gabela'
-
-
-def weater_api_yandx_tomorrow():
-    out_weather = 'Погода на завтра:\n'
-    try:
-        html_text = requests.get(url,params=params,headers = headers).json()
-        for part in parts:
-            temp_min = html_text['forecasts'][1]['parts'][part]['temp_min']
-            temp_max = html_text['forecasts'][1]['parts'][part]['temp_max']
-            feels_like = html_text['forecasts'][1]['parts'][part]['feels_like']
-            f = '{0} {1}ºC...{2}ºC feels like {3}ºC\n'.format(part,temp_min,temp_max,feels_like)
-            out_weather += f
-        return out_weather
-    except BaseException:
-        return 'Gabela'
