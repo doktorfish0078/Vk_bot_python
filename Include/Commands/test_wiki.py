048360fb3c1a2166a0f6581c7152547bbb16da5d
doktorfish0078 <68699739+doktorfish0078@users.noreply.github.com>
2020-09-10 22:03:57 +0400

    1

--- Include/Commands/test_wiki.py
+++ Include/Commands/test_wiki.py
@@ -4,22 +4,23 @@ import wikipedia
 import re
 
 
-
 def wiki_searching(text_msg):
     wikipedia.set_lang("RU")
 
-    query = re.split("[, \-!?:]+", text_msg)
-    querys = wikipedia.search("".join(query[1:]))
+    text_query = re.split("[, \-!?:]+", text_msg)
+    variant_query_from_wiki = wikipedia.search(" ".join(text_query[1:]))
 
-    if not querys:
+    if not variant_query_from_wiki:
         return "Ничего не найдено"
     else:
         try:
-            html_page = requests.get('https://ru.wikipedia.org/wiki/{0}'.format(querys[0])).content
+            html_page = requests.get('https://ru.wikipedia.org/wiki/{0}'.format(variant_query_from_wiki[0])).content
             soup = bs4.BeautifulSoup(html_page, features="html.parser")
             parsed_graph = soup.find('div', {'class', "mw-parser-output"}).find_all('p')
-            return parsed_graph[0].text
+            print(parsed_graph[0].text)
         except BaseException:
             return "Страница не найдена"
+
+
 
 
