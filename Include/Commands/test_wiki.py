eae7f5ac3892da2497e2a9045ca7a9d2131c6c3e
doktorfish0078 <68699739+doktorfish0078@users.noreply.github.com>
2020-09-10 22:11:31 +0400

    Update test_wiki.py

--- Include/Commands/test_wiki.py
+++ Include/Commands/test_wiki.py
@@ -17,7 +17,7 @@ def wiki_searching(text_msg):
             html_page = requests.get('https://ru.wikipedia.org/wiki/{0}'.format(variant_query_from_wiki[0])).content
             soup = bs4.BeautifulSoup(html_page, features="html.parser")
             parsed_graph = soup.find('div', {'class', "mw-parser-output"}).find_all('p')
-            print(parsed_graph[0].text)
+            return parsed_graph[0].text
         except BaseException:
             return "Страница не найдена"
 
