8accf9ab78b2976f39440556a689f6baa8dacecf
liqloi <liqloi@mail.ru>
2020-09-11 20:06:07 +0400

    Update weather.py

--- Include/Commands/weather.py
+++ Include/Commands/weather.py
@@ -9,23 +9,26 @@ def weather(tomorrow = False):
     if tomorrow:
         numb_card_for_parsing = 2
         day = 'завтра'
-    try:
-        html_text = requests.get('https://yandex.ru/pogoda/izhevsk/details?via=ms').text
-        soup = BeautifulSoup(html_text, features="html.parser")
-        weather = soup.find_all('div', {'class', 'card'})
-        weather_condition = weather[numb_card_for_parsing].find_all('td', {'class',
-                                                       'weather-table__body-cell weather-table__body-cell_type_condition'})
-        out_weather = ['Погода на {}:\n'.format(day), weather_condition[2].text]
-        temp = weather[numb_card_for_parsing].find_all('span', {'class', 'temp__value'})
-        for day in range(len(timeday)):
-            timeday[day] += '{}: '.format(weather_condition[day].text, True)
-            for i in range(day * 3, (day + 1) * 3):
-                if day * 3 + 1 == i:
-                    timeday[day] += '...'
-                elif day * 3 + 2 == i:
-                    timeday[day] += ', ощущается как '
-                timeday[day] += temp[i].text + 'ºC'
-            out_weather[0] += timeday[day] + '\n'
-        return out_weather
-    except BaseException:
-        return ['Погода не отвечает, сукааааа', None]
+    # try:
+    html_text = requests.get('https://yandex.ru/pogoda/izhevsk/details?via=ms').text
+    soup = BeautifulSoup(html_text, features="html.parser")
+    weather = soup.find_all('div', {'class', 'card'})
+    weather_condition = weather[numb_card_for_parsing].find_all('td', {'class',
+                                                   'weather-table__body-cell weather-table__body-cell_type_condition'})
+    out_weather = ['Погода на {}:\n'.format(day), weather_condition[2].text]
+    temp = weather[numb_card_for_parsing].find_all('span', {'class', 'temp__value'})
+    for day in range(len(timeday)):
+        timeday[day] += '{}: '.format(weather_condition[day].text, True)
+        for i in range(day * 3, (day + 1) * 3):
+            if day * 3 + 1 == i:
+                timeday[day] += '...'
+            elif day * 3 + 2 == i:
+                timeday[day] += ', ощущается как '
+            timeday[day] += temp[i].text + 'ºC'
+            print(timeday)
+        out_weather[0] += timeday[day] + '\n'
+    return out_weather
+    # except BaseException:
+    #     return ['Погода не отвечает, сукааааа', None]
+
+print(weather())
