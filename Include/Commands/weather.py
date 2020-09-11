daf7ec187bea368a1cb851179f45a734ee4e435b
doktorfish0078 <68699739+doktorfish0078@users.noreply.github.com>
2020-09-11 23:39:14 +0400

    fix weather

--- Include/Commands/weather.py
+++ Include/Commands/weather.py
@@ -1,34 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 
+weather_type = {
+        "Пасмурно": "☁️☁️☁️",
+        "Ясно":  "☀️",
+        "Облачно": "☁️",
+        "Облачно с прояснениями":"⛅️",
+        "Небольшой дождь": "🌧",
+        "Дождь": "🌧☔️🌧",
+        "Малооблачно": "🌤"
+    }
 
 def weather(tomorrow = False):
-    timeday = ['Утром ', 'Днём ', 'Вечером ', 'Ночью ']
+    time_day = ['Утром', 'Днём', 'Вечером', 'Ночью']
     numb_card_for_parsing = 0
     day = 'сегодня'
     if tomorrow:
         numb_card_for_parsing = 2
         day = 'завтра'
-    # try:
-    html_text = requests.get('https://yandex.ru/pogoda/izhevsk/details?via=ms').text
-    soup = BeautifulSoup(html_text, features="html.parser")
-    weather = soup.find_all('div', {'class', 'card'})
-    weather_condition = weather[numb_card_for_parsing].find_all('td', {'class',
-                                                   'weather-table__body-cell weather-table__body-cell_type_condition'})
-    out_weather = ['Погода на {}:\n'.format(day), weather_condition[2].text]
-    temp = weather[numb_card_for_parsing].find_all('span', {'class', 'temp__value'})
-    for day in range(len(timeday)):
-        timeday[day] += '{}: '.format(weather_condition[day].text, True)
-        for i in range(day * 3, (day + 1) * 3):
-            if day * 3 + 1 == i:
-                timeday[day] += '...'
-            elif day * 3 + 2 == i:
-                timeday[day] += ', ощущается как '
-            timeday[day] += temp[i].text + 'ºC'
-            print(timeday)
-        out_weather[0] += timeday[day] + '\n'
-    return out_weather
-    # except BaseException:
-    #     return ['Погода не отвечает, сукааааа', None]
+    try:
+        html_text = requests.get('https://yandex.ru/pogoda/izhevsk/details?via=ms').text
+        soup = BeautifulSoup(html_text, features="html.parser")
+        weather = soup.find_all('div', {'class', 'card'})
 
-print(weather())
+        weather_condition = weather[numb_card_for_parsing].find_all('td', {'class',
+                                                       'weather-table__body-cell weather-table__body-cell_type_condition'})
+        temp = weather[numb_card_for_parsing].find_all('div', {'class', 'weather-table__temp'})
+        temp_feels_like = weather[numb_card_for_parsing].find_all('td', {'class','weather-table__body-cell weather-table__body-cell_type_feels-like'})
+
+        result_weather = ['Погода на {}:\n'.format(day), weather_condition[2].text]
+        for day in range(len(time_day)):
+            result_weather[0] += "{0} {1}, {2}{3}, ощущается как {4}\n".format(
+                time_day[day], temp[day].text, weather_condition[day].text,
+                weather_type[weather_condition[day].text], temp_feels_like[day].text)
+        return result_weather
+    except BaseException:
+        return "Не удалось получить погоду :("
