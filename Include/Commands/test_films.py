b9ce84a19d11055f9ca80397bbd5e201d151e812
doktorfish0078 <68699739+doktorfish0078@users.noreply.github.com>
2020-09-11 23:50:55 +0400

    расширил кол-во фильмов в test_films

--- Include/Commands/test_films.py
+++ Include/Commands/test_films.py
@@ -6,22 +6,25 @@ import requests
 
 def get_films():
     result = ''
-    html_page = requests.get('https://www.kinoafisha.info/rating/movies/?page=0').text
-    soup = bs4.BeautifulSoup(html_page, features="html.parser")
-    films_name = soup.find_all('a', {'class' : 'films_name ref'})
-    about_films = soup.find_all('span', {'class' : 'films_info'})
+    try:
+        html_page1 = requests.get('https://www.kinoafisha.info/rating/movies/?page=0').text
+        html_page2 = requests.get('https://www.kinoafisha.info/rating/movies/?page=1').text
+        soup = bs4.BeautifulSoup(html_page1 + html_page2, features="html.parser")
+        films_name = soup.find_all('a', {'class' : 'films_name ref'})
+        about_films = soup.find_all('span', {'class' : 'films_info'})
 
-    films = []
-    rand_nmbs = [random.randint(0, len(films_name)),random.randint(0, len(films_name)),random.randint(0, len(films_name))]
+        films = []
+        rand_nmbs = [random.randint(0, len(films_name)),random.randint(0, len(films_name)),random.randint(0, len(films_name))]
 
-    for i in range(len(films_name)):
-        films.append("{} | {}, {}, режиссёр:{}".format(films_name[i].text, about_films[i*3].text,
-                                                       about_films[i * 3 + 1].text,  re.sub(r'\s+', ' ', about_films[i * 3 + 2].text )))
+        for i in range(len(films_name)):
+            films.append("{} | {}, {}, режиссёр:{}".format(films_name[i].text, about_films[i*3].text,
+                                                           about_films[i * 3 + 1].text,  re.sub(r'\s+', ' ', about_films[i * 3 + 2].text )))
 
 
-    for pp in rand_nmbs:
-        result += 'Фильм: ' + films[pp] + '\n'
+        for pp in rand_nmbs:
+            result += 'Фильм: ' + films[pp] + '\n'
 
-    return result
-
+        return result
+    except BaseException:
+        return "Не удалось получить фильмы :("
 
